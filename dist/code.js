4242313c77ad687d38d13549b45e96d337010f42
amccook23 <48769922+amccook23@users.noreply.github.com>
2026-09-16 14:26:08 +0000

    Chevron: solid filled SVG, rounded corners, 30% smaller

--- dist/code.js
+++ dist/code.js
@@ -335,9 +335,9 @@
               /* @__PURE__ */ figma.widget.h(
                 SVG,
                 {
-                  width: 13,
-                  height: 8,
-                  src: `<svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L6.5 7L12 1" stroke="${STATUS_FG[item.status]}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`
+                  width: 9,
+                  height: 6,
+                  src: `<svg width="9" height="6" viewBox="0 0 13 8" xmlns="http://www.w3.org/2000/svg"><path d="M 1.5 0.5 L 11.5 0.5 Q 13 0.5 12.2 1.8 L 7.3 7.2 Q 6.5 8.2 5.7 7.2 L 0.8 1.8 Q 0 0.5 1.5 0.5 Z" fill="${STATUS_FG[item.status]}"/></svg>`
                 }
               )
             )
